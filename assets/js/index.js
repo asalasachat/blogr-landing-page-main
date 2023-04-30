621fc8e6bc4e81d239c8fdd7f9e8893530fe6ee6
achsalsa <achsls9206@gmail.com>
2023-04-30 21:43:56 +0200

    rajout des modifications JS pour la prise en charge des hover et click sur les elements du menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,29 @@
 const menuItem=document.querySelectorAll(".dropdown-item");
+let hoverVar=false;
+
+const submenuInHandeler=function (event) {  
+    event.preventDefault();
+    listeElement=this.querySelector("ul");
+    listeElement.classList.remove("undisplay");
+}
+
+const submenuOutHandeler=function (event) {  
+    event.preventDefault();
+    listeElement.classList.add("undisplay");
+    hoverVar=false;
+}
 
 menuItem.forEach(element => {
-    element.addEventListener("click", function (event) {  
-        event.preventDefault();
-        listeElement=this.querySelector("ul");
-        listeElement.classList.remove("undisplay");
+    element.addEventListener("click", submenuInHandeler);
+    element.addEventListener("mouseover", submenuInHandeler);
+    element.addEventListener("mouseleave", function(){
+        setTimeout(() => {
+            if(!hoverVar){
+                console.log("Hello");
+                element.querySelector("ul").classList.add("undisplay");
+            }
+        }, 500);
     });
+    element.querySelector("ul").addEventListener("mouseover",()=>hoverVar=true);
+    element.querySelector("ul").addEventListener("mouseleave", submenuOutHandeler);
 });
